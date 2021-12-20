9b683328acb40e61d71e039b77de66e358a8b604
G-Carneiro <carneiro.gabr@gmail.com>
2021-12-20 13:54:26 -0300

    rename class

--- python/tests/DoublyCircularListTest.py
+++ python/tests/DoublyCircularListTest.py
@@ -1,7 +1,10 @@
+from typing import List, Optional
 import unittest
 
+from ..structures.DoublyCircularList import DoublyCircularList
+from ..structures.Node import DoublyLinkedListNode as Node
 
-class DoublyLinkedListTest(unittest.TestCase):
+
+class DoublyCircularListTest(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
-
